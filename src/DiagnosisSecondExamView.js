ef7938b7a8a59d507ca3e59bd40b513882a64c8e
takimaro1928 <ta11204649@gmail.com>
2025-04-21 22:25:58 +0900

    Update DiagnosisSecondExamView.js

--- src/DiagnosisSecondExamView.js
+++ src/DiagnosisSecondExamView.js
@@ -1,37 +1,19 @@
-import React, { useState } from 'react';
-import { Tabs, TabList, Tab, TabPanel } from './components/Tabs';
-import { FileText, BookOpen, Clock, Calendar } from 'lucide-react';
+import React from 'react';
 import styles from './DiagnosisSecondExamView.module.css';
-import PastExamPractice from './components/PastExamPractice';
+import { BookOpen } from 'lucide-react';
 import FlashcardStudy from './components/FlashcardStudy';
 
 const DiagnosisSecondExamView = () => {
-  const [activeTab, setActiveTab] = useState('practice');
-
   return (
     <div className={styles.container}>
       <h2 className={styles.pageTitle}>中小企業診断士2次試験対策</h2>
       
-      <Tabs activeTab={activeTab} onChange={setActiveTab}>
-        <TabList>
-          <Tab id="practice">
-            <FileText size={18} />
-            <span>過去問練習</span>
-          </Tab>
-          <Tab id="flashcards">
-            <BookOpen size={18} />
-            <span>用語暗記カード</span>
-          </Tab>
-        </TabList>
-        
-        <TabPanel id="practice">
-          <PastExamPractice />
-        </TabPanel>
-        
-        <TabPanel id="flashcards">
-          <FlashcardStudy />
-        </TabPanel>
-      </Tabs>
+      <div className={styles.header}>
+        <BookOpen size={18} />
+        <span>用語暗記カード</span>
+      </div>
+      
+      <FlashcardStudy />
     </div>
   );
 };
