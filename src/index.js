b5db3e7594c5c7e689263bc334802ee50cd208f0
takimaro1928 <ta11204649@gmail.com>
2025-04-26 15:40:46 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import ErrorBoundary from './components/ErrorBoundary';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // StrictModeを一時的に無効化してパフォーマンス問題を修正
-  <App />
+  <ErrorBoundary name="RootApp">
+    <App />
+  </ErrorBoundary>
 );
 
 // サービスワーカーを登録して、オフライン動作を有効にする
