e9f2fd9137fbf9f90828e5e7d3c3bb7545b015c9
takimaro1928 <ta11204649@gmail.com>
2025-04-26 14:35:57 +0900

    Create NotesPage.js

---
+++ src/NotesPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// シンプルなノートページコンポーネント
+function NotesPage() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">ノートページ</h1>
+      <p>このページは開発中です。将来的にはノート機能が追加される予定です。</p>
+    </div>
+  );
+}
+
+export default NotesPage; 
